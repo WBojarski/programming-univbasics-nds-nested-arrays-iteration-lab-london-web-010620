70c339287ee9ab9062572ef9d0a997799a24be98
Wojciech Bojarski <bojawojt@gmail.com>
2019-12-21 00:13:21 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -53,7 +53,7 @@ def total_even_pairs(src)
 
   while row_counter < src.length do
     column_counter = 0 
-    while column_counter < src[row_counter].to_a.length do
+    while column_counter < src[row_counter].to_a.length - 1 do
       
       if src[row_counter][column_counter].to_i.even? && src[row_counter][column_counter + 1].to_i.even?
         total += src[row_counter][column_counter].to_i + src[row_counter][column_counter + 1].to_i
